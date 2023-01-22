7bb2bfe7be3ff68c9c9f3fd9578988091983e09e
anas599 <anas599@yahoo.com>
2023-01-23 00:28:36 +0300

    Add jest to test and pass some functions

---
+++ index.js
@@ -0,0 +1,45 @@
+function stringLength(string){
+    const length=string.length
+    return length>0 && length<10
+}
+
+function reverseString(string1){
+    let arr=[], str=string1.split("")
+    for (let i = str.length-1; i >= 0; i--) {
+        arr.push(str[i])
+    }
+    return arr.join("")
+
+    // return string1.split("").reduce((a,b)=>b+a)
+}
+
+const calculator={
+    add:(a,b)=> a+b,
+    subtract:(a,b)=> a-b,
+    divide: (a,b)=> a/b,
+    multiply: (a,b)=>a*b
+
+}
+
+function capitalize(str){
+    // let arr=str.split(' '),arrstr=[]
+    // for (let i = 0; i < arr.length; i++) {
+    //     arrstr.push(arr[i].charAt(0).toUpperCase()+arr[i].slice(1,arr[i].length))
+    // }
+    // return arrstr.join(' ')
+      let capt= str.replace(/\b[a-z]/gi, function (x) {
+        return x.toUpperCase();
+      });
+      return capt
+}
+
+
+console.log(reverseString('anas'));
+console.log(stringLength('saaaaaaaa'));
+console.log(capitalize('anas sufyan'));
+module.exports = {
+  stringLength,
+  reverseString,
+  calculator,
+  capitalize
+};
---
+++ str.test.js
@@ -0,0 +1,66 @@
+const {
+  stringLength,
+  reverseString,
+  calculator,
+  capitalize,
+} = require("./index.js");
+
+test("string length should be more than 1 and less than 10", () => {
+  expect(stringLength("123456790")).toBe(true);
+});
+
+test("capitalize letter", () => {
+  expect(capitalize("anas sufyan")).toBe('Anas Sufyan');
+});
+
+test("string is reversed", () => {
+  expect(reverseString("anas")).toBe("sana");
+});
+
+describe('calclator',()=>{
+    test('add function',()=>{
+        expect(calculator.add(1,1)).toBe(2)
+    })
+    test('add function',()=>{
+        expect(calculator.add(12,10)).toBe(22)
+    })
+    test('add function',()=>{
+        expect(calculator.add(64,64)).toBe(128)
+    })
+})
+describe('calclator',()=>{
+
+    test('subtract function',()=>{
+        expect(calculator.subtract(3,2)).toBe(1)
+    })
+    test('subtract function',()=>{
+        expect(calculator.subtract(0,0)).toBe(0)
+    })
+    test('subtract function',()=>{
+        expect(calculator.subtract(3,4)).toBe(-1)
+    })
+})
+describe('calclator',()=>{
+
+    test('divide function',()=>{
+        expect(calculator.divide(2,2)).toBe(1)
+    })
+    test('divide function',()=>{
+        expect(calculator.divide(100,5)).toBe(20)
+    })
+    test('divide function',()=>{
+        expect(calculator.divide(10,2)).toBe(5)
+    })
+})
+
+describe("calclator", () => {
+  test("multiply function", () => {
+    expect(calculator.multiply(2, 2)).toBe(4);
+  });
+  test("multiply function", () => {
+    expect(calculator.multiply(100, 5)).toBe(500);
+  });
+  test("multiply function", () => {
+    expect(calculator.multiply(1000, 1)).toBe(1000);
+  });
+});
